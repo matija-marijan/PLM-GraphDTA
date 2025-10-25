b0f0f6bc10347ee65e05573c99f82315566843c0
Matija Marijan <matijamarijan99@gmail.com>
2025-10-25 13:21:51 +0200

    All ontologies preprocessing

--- preprocessing/FRI/deepfri_preprocessing.py
+++ preprocessing/FRI/deepfri_preprocessing.py
@@ -16,7 +16,7 @@ from tqdm import tqdm
 from collections import OrderedDict
 
 model_config = 'preprocessing/FRI/trained_models/model_config.json'
-ont = 'mf'
+onts = ['cc', 'bp', 'mf', 'ec']
 emb_layer = 'global_max_pooling1d'
 
 with open(model_config) as json_file:
@@ -25,38 +25,38 @@ with open(model_config) as json_file:
 params = params['cnn']
 gcn = params['gcn']
 models = params['models']
-predictor = Predictor(models[ont], gcn = gcn)
 
 datasets = ['davis', 'kiba', 'davis_mutation']
 for dataset in datasets:
-    embeddings = []
-    processed_dataset = 'data/' + dataset + '/proteins_deepfri_' + ont + '.json'
-    if not os.path.isfile(processed_dataset):
+    for ont in onts:
+        embeddings = []
+        processed_dataset = 'data/' + dataset + '/proteins_deepfri_' + ont + '.json'
+        if not os.path.isfile(processed_dataset):
 
-        predictor = Predictor(models[ont], gcn=gcn)
-        
-        proteins = json.load(open('data/' + dataset + "/proteins.json"), object_pairs_hook=OrderedDict)
-        prots = []
-        protein_keys = []
-        for t in proteins.keys():
-            prots.append(proteins[t])
-            protein_keys.append(t)
+            predictor = Predictor(models[ont], gcn=gcn)
+            
+            proteins = json.load(open('data/' + dataset + "/proteins.json"), object_pairs_hook=OrderedDict)
+            prots = []
+            protein_keys = []
+            for t in proteins.keys():
+                prots.append(proteins[t])
+                protein_keys.append(t)
 
-        # DeepFRI protein representation
-        for i in tqdm(range(0, len(prots)), desc=f"Processing {dataset} proteins"):
-            prot = prots[i]
-            emb = predictor.predict_embeddings(prot, layer_name = emb_layer)
-            embeddings.append(emb)
+            # DeepFRI protein representation
+            for i in tqdm(range(0, len(prots)), desc=f"Processing {dataset} proteins"):
+                prot = prots[i]
+                emb = predictor.predict_embeddings(prot, layer_name = emb_layer)
+                embeddings.append(emb)
 
-        embeddings = np.asarray(embeddings)
+            embeddings = np.asarray(embeddings)
 
-        df = pd.DataFrame({
-            'protein_key': protein_keys,
-            'sequence': prots,
-            'embedding': [emb.tolist() for emb in embeddings]
-        })
-        df.to_json(processed_dataset, orient='records', indent=4)
+            df = pd.DataFrame({
+                'protein_key': protein_keys,
+                'sequence': prots,
+                'embedding': [emb.tolist() for emb in embeddings]
+            })
+            df.to_json(processed_dataset, orient='records', indent=4)
 
-        print(processed_dataset, ' has been created')
-    else:
-        print(processed_dataset, ' is already created')
+            print(processed_dataset, ' has been created')
+        else:
+            print(processed_dataset, ' is already created')
