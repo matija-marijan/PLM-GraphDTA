2e776f373c091f4175c9f1d34b1f40deeae10045
Matija Marijan <matijamarijan99@gmail.com>
2025-10-27 09:21:44 +0000

    Optimize validation script

--- training_validation.py
+++ training_validation.py
@@ -14,6 +14,8 @@ from models.vnoc_ginconv import Vnoc_GINConvNet
 from models.pdc_vnoc_ginconv import PDC_Vnoc_GINConvNet
 from models.esm_ginconv import ESM_GINConvNet
 from models.fri_ginconv import FRI_GINConvNet
+from models.pdconv_ginconv import PDConv_GINConvNet
+from models.pdconv_vnoc_ginconv import PDConv_Vnoc_GINConvNet
 
 from models.esm_gat import ESM_GATNet
 
@@ -23,17 +25,23 @@ from utils import *
 import argparse
 from tqdm import tqdm
 
+scaler = torch.cuda.amp.GradScaler()
+
 # training function at each epoch
 def train(model, device, train_loader, optimizer, epoch, wandb_log=False):
     # print('Training on {} samples...'.format(len(train_loader.dataset)))
     model.train()
-    for batch_idx, data in tqdm(enumerate(train_loader), total=len(train_loader), leave=False, desc=f"Epoch {epoch}"):
-        data = data.to(device)
+    # for batch_idx, data in tqdm(enumerate(train_loader), total=len(train_loader), leave=False, desc=f"Epoch {epoch}"):
+    for batch_idx, data in enumerate(train_loader):
+        data = data.to(device, non_blocking=True)
         optimizer.zero_grad()
-        output = model(data)
-        loss = loss_fn(output, data.y.view(-1, 1).float().to(device))
-        loss.backward()
-        optimizer.step()
+        
+        with torch.cuda.amp.autocast():
+            output = model(data)
+            loss = loss_fn(output, data.y.view(-1, 1).float().to(device))
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
         # if batch_idx % LOG_INTERVAL == 0:
             # print('Train epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch,
             #                                                                batch_idx * len(data.x),
@@ -46,18 +54,23 @@ def train(model, device, train_loader, optimizer, epoch, wandb_log=False):
 
 def predicting(model, device, loader):
     model.eval()
-    total_preds = torch.Tensor()
-    total_labels = torch.Tensor()
+    total_preds = []
+    total_labels = []
     # print('Make prediction for {} samples...'.format(len(loader.dataset)))
     with torch.no_grad():
-        for data in tqdm(loader, total=len(loader), leave=False, desc="Predicting"):
-            data = data.to(device)
-            output = model(data)
-            total_preds = torch.cat((total_preds, output.cpu()), 0)
-            total_labels = torch.cat((total_labels, data.y.view(-1, 1).cpu()), 0)
+        # for data in tqdm(loader, total=len(loader), leave=False, desc="Predicting"):
+        for data in loader:
+            data = data.to(device, non_blocking=True)
+            with torch.cuda.amp.autocast():
+                output = model(data)
+            total_preds.append(output.cpu())
+            total_labels.append(data.y.view(-1, 1).cpu())
+            
+    total_preds = torch.cat(total_preds, dim=0)
+    total_labels = torch.cat(total_labels, dim=0)
     return total_labels.numpy().flatten(),total_preds.numpy().flatten()
 
-datasets = ['davis', 'kiba']
+datasets = ['davis', 'kiba', 'davis_mutation']
 
 all_models = {
     'GINConvNet': GINConvNet, 
@@ -69,7 +82,9 @@ all_models = {
     'ESM_GINConvNet': ESM_GINConvNet, 
     'FRI_GINConvNet': FRI_GINConvNet, 
     'PDC_Vnoc_GINConvNet': PDC_Vnoc_GINConvNet,
-    'ESM_GATNet': ESM_GATNet
+    'ESM_GATNet': ESM_GATNet,
+    'PDConv_GINConvNet': PDConv_GINConvNet,
+    'PDConv_Vnoc_GINConvNet': PDConv_Vnoc_GINConvNet
 }
 
 parser = argparse.ArgumentParser(description="Run a specific model on a specific dataset.")
@@ -88,10 +103,14 @@ parser.add_argument('-vf', '--validation_fold', type=int, default=0,
                     help="Fold index to use for validation when using k-fold cross-validation (default: 0).")
 # parser.add_argument('--split_type', type=str, default=None,
 #                     help="Type of data split. Choose from: 'random', 'original', 'kfold', 'protein_cold', 'drug_cold', or 'fully_cold'.")
-parser.add_argument('--mutation', action='store_true', default=False,
-                    help="Flag for including protein sequence mutations for the Davis dataset (default: False).")
+# parser.add_argument('--mutation', action='store_true', default=False,
+#                     help="Flag for including protein sequence mutations for the Davis dataset (default: False).")
 parser.add_argument('--num_layers', type=int, default=3,
                     help="Number of layers in the protein learning channel (default: 3).")
+parser.add_argument('--kernel_size', type=int, default=8,
+                    help="Convolution filter kernel size for convolutional models (default: 8)")
+parser.add_argument('--description', type=str, default=None,
+                    help="Description to add to run and/or group name for logging (default: None).")
 
 args = parser.parse_args()
 
@@ -134,15 +153,16 @@ TEST_BATCH_SIZE = 512
 LR = 0.0005
 LOG_INTERVAL = 20
 NUM_EPOCHS = 1000
+NUM_WORKERS = 24
 
 print('Learning rate: ', LR)
 print('Epochs: ', NUM_EPOCHS)
 
-group_name = f"{args.model}_{args.dataset}_{args.num_layers}_layers"
-run_name = f"{args.model}_{args.dataset}_{args.num_layers}_layers"
-if args.mutation:
-    group_name += "_mutation"
-    run_name += "_mutation"
+group_name = f"{args.model}_{args.dataset}_{args.num_layers}_layers_{args.kernel_size}_kernel"
+run_name = f"{args.model}_{args.dataset}_{args.num_layers}_layers_{args.kernel_size}_kernel"
+if args.description is not None:
+    run_name += f"_{args.description}"
+    group_name += f"_{args.description}"
 if args.seed is not None:
     run_name += f"_seed_{args.seed}"
 run_name += f"_fold_{args.validation_fold}"
@@ -153,7 +173,8 @@ if args.wandb:
 # Main program: Train on specified dataset 
 if __name__ == "__main__":
     print('Training ' + model_st + ' on ' + dataset + ' dataset...')
-    dta_dataset = DTADataset(root='data', dataset=dataset, target_type=target_type, mutation=args.mutation)
+    # dta_dataset = DTADataset(root='data', dataset=dataset, target_type=target_type, mutation=args.mutation)
+    dta_dataset = DTADataset(root='data', dataset=dataset, target_type=target_type)
 
     # original k-fold split (hard coded!)
     all_folds = [0, 1, 2, 3, 4]
@@ -166,18 +187,22 @@ if __name__ == "__main__":
     test_dataset = dta_dataset[dta_dataset._data.fold == -1]
 
     # make data PyTorch mini-batch processing ready
-    train_loader = DataLoader(train_dataset, batch_size=TRAIN_BATCH_SIZE, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=TEST_BATCH_SIZE, shuffle=False)
-    val_loader = DataLoader(val_dataset, batch_size=TEST_BATCH_SIZE, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=TRAIN_BATCH_SIZE, shuffle=True, pin_memory = True, num_workers = NUM_WORKERS)
+    test_loader = DataLoader(test_dataset, batch_size=TEST_BATCH_SIZE, shuffle=False, pin_memory = True, num_workers = NUM_WORKERS//2)
+    val_loader = DataLoader(val_dataset, batch_size=TEST_BATCH_SIZE, shuffle=False, pin_memory = True, num_workers = NUM_WORKERS//2)
 
     # training the model
     device = torch.device(cuda_name if torch.cuda.is_available() else "cpu")
-    model = modeling(num_layers=args.num_layers).to(device)
+    model = modeling(num_layers=args.num_layers, kernel_size=args.kernel_size).to(device)
     loss_fn = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=LR)
 
     best_mse = 1000
     best_epoch = -1
+    
+    model_file_name = 'trained_models/model_' + run_name + '_validation.pt'
+    result_file_name = 'trained_models/result_' + run_name + '_validation.csv'
+    os.makedirs('trained_models', exist_ok=True)
 
     for epoch in range(NUM_EPOCHS):
         tqdm.write(f'\nEpoch {epoch+1}')
@@ -189,18 +214,14 @@ if __name__ == "__main__":
             wandb.log({"rmse": ret[0], "mse": ret[1], "pearson": ret[2], "spearman": ret[3]})
 
         if ret[1]<best_mse:
-            best_model = model
+            torch.save(model.state_dict(), model_file_name)
             best_epoch = epoch+1
             best_mse = ret[1]
         tqdm.write(f'Validation MSE: {ret[1]:.6f}\nBest MSE: {best_mse:.6f} (epoch {best_epoch})')
 
-    model_file_name = 'trained_models/model_' + run_name + '_validation.model'
-    result_file_name = 'trained_models/result_' + run_name + '_validation.csv'
-    os.makedirs('trained_models', exist_ok=True)
+    model.load_state_dict(torch.load(model_file_name))
 
-    torch.save(best_model.state_dict(), model_file_name)
-
-    G,P = predicting(best_model, device, val_loader)
+    G,P = predicting(model, device, val_loader)
     val_ret = [rmse(G,P),mse(G,P),pearson(G,P),spearman(G,P),ci(G,P)]
 
     tqdm.write('\nResults on val set:')
@@ -210,15 +231,15 @@ if __name__ == "__main__":
     tqdm.write(f"Spearman: {val_ret[3]}")
     tqdm.write(f"CI: {val_ret[4]}\n")
 
-    G,P = predicting(best_model, device, test_loader)
-    test_ret = [rmse(G,P),mse(G,P),pearson(G,P),spearman(G,P),ci(G,P)]
+#     G,P = predicting(model, device, test_loader)
+#     test_ret = [rmse(G,P),mse(G,P),pearson(G,P),spearman(G,P),ci(G,P)]
 
-    tqdm.write('\nResults on test set:')
-    tqdm.write(f"RMSE: {test_ret[0]}")
-    tqdm.write(f"MSE: {test_ret[1]}")
-    tqdm.write(f"Pearson: {test_ret[2]}")
-    tqdm.write(f"Spearman: {test_ret[3]}")
-    tqdm.write(f"CI: {test_ret[4]}")
+#     tqdm.write('\nResults on test set:')
+#     tqdm.write(f"RMSE: {test_ret[0]}")
+#     tqdm.write(f"MSE: {test_ret[1]}")
+#     tqdm.write(f"Pearson: {test_ret[2]}")
+#     tqdm.write(f"Spearman: {test_ret[3]}")
+#     tqdm.write(f"CI: {test_ret[4]}")
 
     if args.wandb:
         wandb.log({
@@ -226,22 +247,22 @@ if __name__ == "__main__":
             "val_mse": val_ret[1],
             "val_pearson": val_ret[2],
             "val_spearman": val_ret[3],
-            "val_ci": val_ret[4],
-            
-            "test_rmse": test_ret[0],
-            "test_mse": test_ret[1],
-            "test_pearson": test_ret[2],
-            "test_spearman": test_ret[3],
-            "test_ci": test_ret[4]
+            "val_ci": val_ret[4]            
+#             "test_rmse": test_ret[0],
+#             "test_mse": test_ret[1],
+#             "test_pearson": test_ret[2],
+#             "test_spearman": test_ret[3],
+#             "test_ci": test_ret[4]
         })
         wandb.finish()
 
     with open(result_file_name, 'w') as f:
         # write header
-        f.write("test_rmse,test_mse,test_pearson,test_spearman,test_ci,val_rmse,val_mse,val_pearson,val_spearman,val_ci\n")
+        # f.write("test_rmse,test_mse,test_pearson,test_spearman,test_ci,val_rmse,val_mse,val_pearson,val_spearman,val_ci\n")
+        f.write("val_rmse,val_mse,val_pearson,val_spearman,val_ci\n")
         # write values
-        f.write(','.join(map(str, test_ret)))
-        f.write(',')
+        # f.write(','.join(map(str, test_ret)))
+        # f.write(',')
         f.write(','.join(map(str, val_ret)))
         f.write('\n')
     
